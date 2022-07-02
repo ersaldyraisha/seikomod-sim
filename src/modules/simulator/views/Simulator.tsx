7f66fc6ac6cf3592dd3bff4b660edb4ac1497488
Andi Ersaldy Raisha Pakki <andi.ersaldy@gmail.com>
2022-07-02 16:22:24 +0700

    feat: use fixed layout

--- src/modules/simulator/views/Simulator.tsx
+++ src/modules/simulator/views/Simulator.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
 import resetIcon from '../../../assets/images/icon-reset.png'
+import infoIcon from '../../../assets/images/icon-info.png'
 import { useState, useEffect } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { getItems, getCategories, categoryIds, Item } from '../utils'
@@ -10,18 +11,61 @@ function Simulator() {
   
   return (
     <div className={classNames(
-      'flex flex-col h-[100vh] justify-between items-center overflow-hidden',
+      'h-[100vh] overflow-hidden',
       {'bg-zinc-800 !text-zinc-200': isDarkMode}
     )}>
-      <TitleBar/>
+      <Navbar/>
       <Result />
-      <Picker />
-      <Navbar />
+      <Items />
+      <Category />
     </div>  
   )
 }
 
-function Navbar() {
+function ItemsInfo() {
+  const [isActive, setActive] = useState(false)
+  const activeItems = useSimulator(state => state.activeItems)
+  const isDarkMode = useSimulator(state => state.isDarkMode)
+
+  const itemsList = categoryIds
+    .filter((item) => !!activeItems[item])
+    .map((item) => {
+      return (
+        <li className="mb-2 last:mb-0">
+          <p className="text-xs">{`${item[0].toUpperCase()}${item.substring(1)}`}</p>
+          <p className="text-sm font-bold">{activeItems[item]?.name}</p>
+        </li>
+      )
+    })
+
+  return (
+    <button 
+      className="absolute top-8 right-full mr-5 w-10 h-10 p-2 rounded-full bg-zinc-700"
+      onMouseEnter={() => setActive(true)}
+      onMouseLeave={() => setActive(false)}
+    >
+      <AnimatePresence>
+        {isActive && (
+          <motion.ul
+            className={classNames(
+              'absolute bottom-[calc(100%+10px)] left-1/2 min-w-[120px] p-3 text-left bg-zinc-200 text-red rounded-lg cursor-default pointer-events-none',
+              { '!bg-zinc-700' : isDarkMode }
+            )}
+            onClick={(e) => e.stopPropagation()}
+            initial={{ opacity: 0, y: 20, x: '-50%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+          >
+            {itemsList}
+          </motion.ul>
+        )}
+      </AnimatePresence>
+      <img src={infoIcon} alt="reset" />
+    </button>
+  )
+}
+
+function Category() {
   const navItems = getCategories()
   const isDarkMode = useSimulator(state => state.isDarkMode)
   const activeNavId = useSimulator(state => state.activeCategoryId)
@@ -34,7 +78,7 @@ function Navbar() {
       <button
         className={
           classNames(
-            'relative px-3 py-1 rounded-full text-black font-bold cursor-pointer',
+            'relative px-3 py-1 rounded-full text-black font-bold cursor-pointer whitespace-nowrap',
             { 'outline:animate-ping': true},
             { 'bg-blue-600 !text-white': activeNavId === nav.id },
             { '!text-zinc-200': isDarkMode}
@@ -68,23 +112,26 @@ function Navbar() {
   ))
   
   return(
-    <nav className="pb-10 pt-7 relative z-10">
+    <nav className="fixed bottom-0 left-1/2 z-10 pb-10 pt-7 translate-x-[-50%]">
       <ul className={classNames(
-        'flex justify-center bg-zinc-200 gap-[20px] rounded-full p-2',
+        'flex justify-center bg-zinc-200 gap-[20px] rounded-full p-2 mx-auto',
         { '!bg-zinc-900': isDarkMode }
       )}>
         {renderedNavItems}
       </ul>
       {!!activeItems.case && (
-        <button className="absolute top-8 left-full ml-5 w-10 h-10 p-2 rounded-full bg-zinc-700" onClick={() => resetActiveItems()}>
-          <img src={resetIcon} alt="reset" />
-        </button>
+        <>
+          <button className="absolute top-8 left-full ml-5 w-10 h-10 p-2 rounded-full bg-zinc-700" onClick={() => resetActiveItems()}>
+            <img src={resetIcon} alt="reset" />
+          </button>
+          <ItemsInfo/>
+        </>
       )}
     </nav>
   )
 }
 
-function Picker() {
+function Items() {
   const [page, setPage] = useState(1)
   const [items, setItems] = useState<Array<Item>>([])
   
@@ -153,8 +200,8 @@ function Picker() {
       { activeCategoryId && (
         <motion.div 
           className={classNames(
-            'flex justify-center items-center gap-3 relative mt-auto pt-4 min-h-[242px] border-t border-t-zinc-200 w-full',
-            { '!border-t-zinc-700': isDarkMode }
+            'flex justify-center items-center gap-3 fixed bottom-[105px] pt-4 w-full min-h-[242px] border-t border-t-zinc-200 bg-white',
+            { '!border-t-zinc-700 !bg-zinc-800': isDarkMode }
           )}
           initial={{ y: 250, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
@@ -220,7 +267,7 @@ function Result() {
   
   return (
     <motion.div
-      className="flex items-center"
+      className="flex items-center justify-center mx-auto"
       animate={!!activeCategoryId ? 'open' : 'closed'}
       variants={{
         open: { opacity: 1, y: 100 },
@@ -238,7 +285,7 @@ function Result() {
   )
 }
 
-function TitleBar() {
+function Navbar() {
   const isDarkMode = useSimulator(state => state.isDarkMode)
   const toggleDarkMode = useSimulator(state => state.toggleDarkMode)
 
@@ -249,36 +296,38 @@ function TitleBar() {
   }, [])
 
   return (
-    <div className="flex fixed justify-between pt-8 w-[1200px]">
-      <h1 className={classNames(
-        'self-start font-title text-blue-600',
-        { '!text-zinc-200': isDarkMode }
-      )}>
-        <span className='block text-xl leading-none'>
-          The
-        </span> 
-        <span className='block text-5xl font-black leading-none tracking-tight'>
-          Seikonator
-        </span> 
-      </h1>
-      <button onClick={() => toggleDarkMode()}>
-        <div className={classNames(
-          'flex bg-zinc-200 p-1 rounded-full w-[45px]',
-          {'!bg-blue-600': !isDarkMode},
+    <div className="fixed w-full">
+      <div className="flex justify-between pt-8 mx-auto w-[1200px]">
+        <h1 className={classNames(
+          'self-start font-title text-blue-600',
+          { '!text-zinc-200': isDarkMode }
         )}>
-          <motion.div
-            animate={isDarkMode ? 'off' : 'on'}
-            variants={{
-              on: { x: 20 },
-              off: { x: 0 }
-            }}
-            className={classNames(
-              'w-4 h-4 bg-white rounded-full',
-              {'!bg-black': isDarkMode}
-            )}
-          />
-        </div>
-      </button>
+          <span className='block text-xl leading-none'>
+            The
+          </span> 
+          <span className='block text-5xl font-black leading-none tracking-tight'>
+            Seikonator
+          </span> 
+        </h1>
+        <button onClick={() => toggleDarkMode()}>
+          <div className={classNames(
+            'flex bg-zinc-200 p-1 rounded-full w-[45px]',
+            {'!bg-blue-600': !isDarkMode},
+          )}>
+            <motion.div
+              animate={isDarkMode ? 'off' : 'on'}
+              variants={{
+                on: { x: 20 },
+                off: { x: 0 }
+              }}
+              className={classNames(
+                'w-4 h-4 bg-white rounded-full',
+                {'!bg-black': isDarkMode}
+              )}
+            />
+          </div>
+        </button>
+      </div>
     </div>
   )
 }
